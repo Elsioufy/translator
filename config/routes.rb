e0cc134d9d3fd42dd98083fd1d558d93ce896316
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-21 05:29:49 +0200

    Generate articles controller for show and index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Ckeditor::Engine => '/ckeditor'
+  resources :articles, only: [:show, :index]
+  root :to => "articles#index"
 end
