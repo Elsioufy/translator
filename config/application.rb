45acba55d406beea1bbd58cd2277e703aa9cd5de
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-23 07:58:15 +0200

    Bug Fix of precompile issue with heroki

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Phraseapp
     config.active_record.observers = [:article_observer]
     config.i18n.default_locale = :en
     I18n.available_locales = [:en, :de]
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
