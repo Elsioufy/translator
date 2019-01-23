2bb2d3752bcfae4f041c7fc32ca0582e22752b4a
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-23 06:31:34 +0200

    Add articles controller spec & add trait to factory article

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe ArticlesController, type: :controller do
-
+describe ArticlesController, type: :controller do
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+  describe "GET show" do
+    it "has a 200 status code" do
+      article = create(:article)
+      get :show, params: {id: article.id}
+      expect(response.status).to eq(200)
+    end
+  end
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :article do
     question { Faker::Lorem.paragraph(1) }
     answer { Faker::Lorem.paragraph(100) }
+    trait :simple do
+      question { "Question?" }
+      answer { "Answer" }
+    end
   end
 end
